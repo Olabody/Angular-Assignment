277527a59de57f432c7b6054c6869ff9946f67cc
Olabody <adamsolabody@gmail.com>
2024-07-25 14:21:13 +0100

    Angular Assignment

---
+++ src/app/staff.ts
@@ -0,0 +1,7 @@
+import { Staff } from './list';
+
+export const STAFF: Staff[] = [
+  { name: 'Bode', job: 'Web Developer' },
+  { name: 'Okiki', job: 'Product Designer' },
+  { name: 'Bambi', job: 'Software Tester' },
+];
